f9e24a68d355fbe65504f26f63b8dce68d1bd71e
pesuts <daniel.sinaga.ds@gmail.com>
2024-12-23 23:56:48 +0700

    chore: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Poppins, Poetsen_One  } from "next/font/google";
+import { Poppins, Poetsen_One } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
 import Footer from "@/components/landing-page/Footer";
@@ -6,19 +6,33 @@ import Footer from "@/components/landing-page/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portal UMKM - Desa Tanjungrejo",
+  description: "Portal UMKM - Desa Tanjungrejo",
+  authors: [{ name: "KKN 82 UPN", url: process.env.NEXT_PUBLIC_API_URL }],
+  icons: {
+    icon: '/kebumen.ico'
+  },
+  openGraph: {
+    title: "Portal UMKM - Desa Tanjungrejo",
+  },
+}
+
 const poppins = Poppins({
-  subsets: ['latin'], 
-  weight: ['400', '600', '700'], 
-  variable: '--font-poppins', 
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-poppins",
+  display: "swap",
 });
 
 const poetsenOne = Poetsen_One({
-  subsets: ['latin'], 
-  weight: ['400'],
-  variable: '--font-poetsen-one', 
-  display: 'swap',
-  adjustFontFallback: false
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-poetsen-one",
+  display: "swap",
+  adjustFontFallback: false,
 });
 
 export default function RootLayout({
